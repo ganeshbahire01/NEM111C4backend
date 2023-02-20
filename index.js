d8d11c711617e4e1ef6dd3cedcff10d2e657d8d6
BaZuKaGanesh1 <ganesh12345bahire@gmail.com>
2023-02-20 16:44:18 +0530

    asd

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const { connection } = require("./db");
 const { userRoutes } = require("./Routes/User.route");
-const { Auth } = require("./middelware/authmiddelware");
 const { postRoutes } = require("./Routes/Post.route");
+const { Auth } = require("./middelware/authmiddelware");
 const app = express();
-// const {auth}
 require("dotenv").config();
 app.use(express.json());
 app.use(cors());
-app.use("/user", userRoutes);
 app.use(Auth);
+app.use("/user", userRoutes);
 app.use("/post", postRoutes);
 
 app.listen(process.env.PORT, async () => {
